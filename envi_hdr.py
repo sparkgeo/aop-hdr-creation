76581340fa205d0dbb02a573f5930518339fdc61
Michael Connor <mike@sparkgeo.com>
2016-07-24 20:51:18 -0700

    Rename envi_hdr file.

--- envi_hdr.py
+++ envi_hdr.py
@@ -4,8 +4,7 @@ import collections
 from glob import glob
 from shutil import copyfile
 
-from datetime import date
-
+RGB_BANDS = ['R', 'G', 'B']
 
 DG_SATID_TO_ENVI = {
     'WV01': 'WorldView-1',
@@ -18,48 +17,58 @@ DG_SATID_TO_ENVI = {
 
 DG_WAVELENGTH_UNITS = 'nm'
 
-def create_hdr(aop_path, output_port_path, debug="no", description=None, **kwargs):
+def create_hdr(img_path, output_port_path, **kwargs):
     logit = kwargs.get('logger')
-    #aop_path is assumed to be a path to the .tif file of an AOP image product
-    filename = os.path.split(aop_path)[1]
+    #img_path is must be a path to the .tif file of an AOP image product
+    filename = os.path.split(img_path)[1]
     new_filename = '%s.hdr' % os.path.splitext(filename)[0]
 
-    logit.debug('Search Pattern: %s' % ('%s.*' % os.path.splitext(aop_path)[0]))
-    logit.debug('Input Files %s' % glob('%s.*' % os.path.splitext(aop_path)[0]))
+    try:
+        os.makedirs(output_port_path)
+    except:
+        pass
+
     # Copy input files to output
-    for filename in glob('%s.*' % os.path.splitext(aop_path)[0]):
+    for filename in glob('%s.*' % os.path.splitext(img_path)[0]):
         dest = os.path.join(output_port_path, os.path.split(filename)[1])
         copyfile(filename, dest)
         logit.debug('%s -> %s' % (filename, dest))
 
     #create empty hdr file
     hdr_file = open(os.path.join(output_port_path, new_filename), "w+")
-    logit.debug('New log file: %s' % hdr_file)
+    logit.debug('New hdr file: %s' % hdr_file)
 
     # Add fixed values hdr line
     hdr_file.write('ENVI\n')
 
     #open image in geoio
-    img = geoio.DGImage(aop_path)
-
-    logit.debug('Image Summary: %s' % img.print_img_summary)
+    try:
+        img = geoio.DGImage(img_path)
+    except TypeError:
+        # Not an ortho image
+        img = geoio.GeoImage(img_path)
 
     #create ordered dictto have some contraol over writing order
     envi_dict = collections.OrderedDict()
     #add elements to the ODict
-    envi_dict['description'] = '{Creating ENVI hdr file from AOP data [%s]}'
+    envi_dict['description'] = '{Creating ENVI hdr file from AOP data}'
     envi_dict['sensor type'] = DG_SATID_TO_ENVI[img.meta.satid]
     envi_dict['lines'] = str(img.meta.shape[1])
     envi_dict['samples'] = str(img.meta.shape[2])
     envi_dict['bands'] = str(img.meta.shape[0])
-    envi_dict['band names'] = '{%s}' % ', '.join(str(e) for e in img.meta.band_names)
-    envi_dict['wavelength'] = '{%s}' % ', '.join(str(e) for e in img.meta.band_centers)
+    if envi_dict['bands'] == '3': # Condition for RGB images??
+        band_indexes = [i for i, v in enumerate(img.meta.band_names) if v in RGB_BANDS]
+        band_wavelengths = [img.meta.band_centers[index] for index in band_indexes]
+        envi_dict['band_names'] = '{%s}' % ', '.join(RGB_BANDS)
+        envi_dict['wavelength'] = '{%s}' % ', '.join(str(e) for e in band_wavelengths)
+    else:
+        envi_dict['band names'] = '{%s}' % ', '.join(str(e) for e in img.meta.band_names)
+        envi_dict['wavelength'] = '{%s}' % ', '.join(str(e) for e in img.meta.band_centers)
     envi_dict['wavelength units'] = DG_WAVELENGTH_UNITS
 
     for entry, value in envi_dict.iteritems():
         #iterate through elements to write them out to file
         hdr_file.write('%s = %s\n' % (entry, value))
 
-    logit.debug('Finished writing HDR file.')
     #close file
     hdr_file.close()
